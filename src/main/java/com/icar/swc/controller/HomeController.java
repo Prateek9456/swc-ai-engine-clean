cb2fc0a3ca7a3c5c62ec68ab6048fb25b3492cd4
mayank2OP <mayankrawat460@gmail.com>
2026-01-29 20:34:25 +0530

    Update HomeController.java

--- src/main/java/com/icar/swc/controller/HomeController.java
+++ src/main/java/com/icar/swc/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.icar.swc.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "home"; // maps to home.ejs
+        return "SWC DSS Backend is running";
     }
 }
